07d5773f7f8537e33fda74c6f003bab00fa79fc8
atgctg <105969161+atgctg@users.noreply.github.com>
2026-09-23 16:56:55 +0200

    fix: strip whitespace

--- src/client/caption.ts
+++ src/client/caption.ts
@@ -3,4 +3,6 @@ export function stripSpeechTags(text: string): string {
     .replace(/\[laughter\]/gi, '')
     .replace(/<(speed|volume|emotion|break|spell)\b[^>]*\/>/gi, ' ')
     .replace(/<\/?(speed|volume|emotion|break|spell)\b[^>]*>/gi, '')
+    .replace(/^[ \t]+/gm, '')
+    .replace(/[ \t]{2,}/g, ' ')
 }
